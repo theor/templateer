5255d57bd44ddc3103d3cc0eb6a94e0192ae8f54
Theo Richart <theor@unity3d.com>
2022-12-27 21:25:21 -0500

    poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,136 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, Suspense } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import "bulma/css/bulma.min.css";
 import * as Bulma from 'react-bulma-components';
 import { emit, listen, UnlistenFn, EventCallback } from '@tauri-apps/api/event';
- import * as Papa from 'papaparse';
+import * as Papa from 'papaparse';
+
+type Project = {
+  file: string;
+  csv: string;
+  template: string;
+}
 // Check if the `$APPDATA/avatar.png` file exists
-let listenCallback: EventCallback<string>|undefined = undefined;
+function doTemplate(tpl: string, data: string[][], dataRow: number): Promise<string> {
+  if (!data || data.length <= 1)
+    return Promise.resolve("<no data>");
+  var headers = data[0];
+  var row = data[dataRow];
+  let res = tpl;
+  headers.forEach((h, i) => res = res.replaceAll(`$${h}`, row[i]));
+  return invoke<string>("render", { markdown: res });
 
+}
+let listenCallback: ((path: string) => void) | undefined = undefined;
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [csvContent, setCsvContent] = useState<unknown>([]);
-  const [unlistenFn, setUnlistenFn] = useState<UnlistenFn|undefined>(undefined);
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("cwd"));
-
-  }
-
-  useEffect(() => {
-    (async() => {
-    const unlisten = await listen<string>('fs', e => {
-      if(listenCallback)
-        listenCallback(e);
-    });
-    setUnlistenFn(unlisten);
-  })();
-  return typeof unlistenFn === 'function' ? () => unlistenFn() : undefined;
+  console.log("rec")
+  const unlisten = listen<string>('fs', e => {
+    if (listenCallback)
+      listenCallback(e.payload);
   });
+  return <Suspense fallback={<span>Loading</span>}>
+    <App2 unlisten={unlisten} />
+  </Suspense>;
+}
+
+interface PanelProps {
+  title: string;
+  children: React.ReactNode;
+}
+const Panel = ({ title, children }: PanelProps) => {
+  const [opened, setOpened] = useState(true);
+  return <Bulma.Columns.Column narrow={!opened}>
+    {opened ? <>
+      <div className="rotated-wrapper">
+        <span onClick={() => setOpened(!opened)} className="has-background-light">{title}</span>
+      </div>
+      {children} </> :
+      <div className="rotated-wrapper">
+        <span onClick={() => setOpened(!opened)} className="rotated has-background-light">{title}</span>
+      </div>}
+  </Bulma.Columns.Column>
+
+}
+function App2({ unlisten }: { unlisten: Promise<UnlistenFn> }) {
+  // if(typeof unlisten === 'function')
+  // unlisten();
+  console.log(unlisten)
+  const [row, setRow] = useState(1);
+  const [template, setTemplate] = useState("# $Nom\n\n$Note");
+  const [preview, setPreview] = useState("");
+  const [csvContent, setCsvContent] = useState<string[][]>([]);
+  // const [unlistenFn, setUnlistenFn] = useState<UnlistenFn|undefined>(undefined);
+
+
+
+  // useEffect(() => {
+  //   (async() => {
+  //   const unlisten = await listen<string>('fs', e => {
+  //     if(listenCallback)
+  //       listenCallback(e);
+  //   });
+  //   setUnlistenFn(unlisten);
+  // })();
+  // return typeof unlistenFn === 'function' ? () => unlistenFn() : undefined;
+  // });
 
   listenCallback = async (e) => {
     console.warn(e);
-    const content = await invoke<string>("getfile", {name: e.payload});
-    const parsed = Papa.parse(content, {header:true,delimiter: ','});
+    const content = await invoke<string>("getfile", { name: e });
+    const parsed = Papa.parse<string[]>(content, { delimiter: ',' });
     // const content = await invoke<string>("greet", {name: e.payload});
     console.log("content", content, parsed);
     setCsvContent(parsed.data);
   };
+  useEffect(() => {
+    // if(listenCallback)
+      // listenCallback("../asd.csv");
+  });
+  useEffect(() => {
+    doTemplate(template, csvContent, row).then(x => setPreview(x));
+  }, [template, row])
 
   // greet();
 
 
   return (
+
     <Bulma.Section>
-      <Bulma.Container>
-        <Bulma.Columns>
-          <Bulma.Columns.Column>
+      <Bulma.Columns>
+        <Panel title="Data">
           <Bulma.Block>
-            <Bulma.Content>
-              {JSON.stringify(csvContent)}
-              </Bulma.Content>
-            </Bulma.Block>
-          </Bulma.Columns.Column>
-          <Bulma.Columns.Column>
-            <Bulma.Block>
-              {greetMsg}
-            </Bulma.Block>
-          </Bulma.Columns.Column>
-        </Bulma.Columns>
-      </Bulma.Container>
+            <Bulma.Table striped size="fullwidth" hoverable>
+              <thead>
+                {csvContent?.length > 0 && <tr>{csvContent[0].map((f, i) => <th key={i}>{f}</th>)}</tr>}
+              </thead>
+              <tbody>
+                {csvContent && csvContent.map((crow, i) => i == 0
+                  ? undefined
+                  : <tr className={`${i === row && 'is-selected'}`} onClick={_ => setRow(i)} key={i}>{crow.map((f, j) => <td key={j}>{f}</td>)}</tr>
+                )}
+              </tbody>
+            </Bulma.Table>
+          </Bulma.Block>
+        </Panel>
+        
+        <Panel title="template">
+          <Bulma.Block>
+            <Bulma.Form.Textarea value={template} onChange={e => setTemplate(e.target.value)}>
+            </Bulma.Form.Textarea>
+          </Bulma.Block>
+
+        </Panel>
+        <Panel title="Preview">
+          <Bulma.Block>
+            <Bulma.Content dangerouslySetInnerHTML={{ __html: preview }}>
+            </Bulma.Content>
+          </Bulma.Block>
+        </Panel>
+      </Bulma.Columns>
     </Bulma.Section>
-    // <div className="container">
-    //   <h1>Welcome to Tauri!</h1>
-
-    //   <div className="row">
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://tauri.app" target="_blank">
-    //       <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-    //     </a>
-    //     <a href="https://reactjs.org" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-
-    //   <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-    //   <div className="row">
-    //     <div>
-    //       <input
-    //         id="greet-input"
-    //         onChange={(e) => setName(e.currentTarget.value)}
-    //         placeholder="Enter a name..."
-    //       />
-    //       <button type="button" onClick={() => greet()}>
-    //         Greet
-    //       </button>
-    //     </div>
-    //   </div>
-    //   <p>{greetMsg}</p>
-    // </div>
+   
   );
 }
 
